007221f9770965a1e551e40ad42977d581166d13
Ignacio Nogueras <noguerasnacho@gmail.com>
2023-01-29 19:54:46 -0300

    HomeWork 1 done

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,9 +1,24 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   if(typeof num != 'string') return "Num tiene que ser un string";
+   let result = 0;
+   num = num.split('').reverse();
+   for(var i = 0; i < num.length; i++){
+      result = result + Math.pow(2, i) * num[i];
+   }
+   return result;
+}
 
 function DecimalABinario(num) {
-   //comentario
+   if(typeof num != 'number') return "num tiene que ser un number";
+   let bin = [];
+   while(num > 1){
+      bin.unshift(num%2);
+      num = Math.floor(num/2);
+   }
+   bin.push(num);
+   return bin.reverse().join('');
 }
 
 module.exports = {
